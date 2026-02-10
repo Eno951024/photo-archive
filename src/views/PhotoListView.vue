7524c03e504d22a55ba8975a912fc337e2b28d1e
Eno951024 <kamo8352@gmail.com>
2026-02-10 19:02:31 +0900

    複数画像対応

--- src/views/PhotoListView.vue
+++ src/views/PhotoListView.vue
@@ -13,22 +13,60 @@
           elevation="2"
           @click="openPhoto(photo)"
         >
-          <v-img
-            :src="photo.image"
-            height="200"
-            cover
-          />
-
+          <v-img :src="photo.images[0]" height="200" cover />
           <v-card-title class="text-h6">{{ photo.title }}</v-card-title>
           <v-card-subtitle class="text-subtitle-1">{{ photo.date }}</v-card-subtitle>
         </v-card>
       </v-col>
     </v-row>
+
+    <v-dialog
+      v-model="showModal"
+      max-width="700"
+      persistent
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="modal-card">
+       <div 
+          @mouseenter="hovering = true"
+          @mouseleave="hovering = false"
+        >
+          <v-carousel
+            v-if="selectedPhoto"
+            hide-delimiter-background
+            :show-arrows="hovering && selectedPhoto.images.length > 1 && smAndUp"
+            height="400"
+            :cycle="false"
+            :continuous="false"
+          >
+            <v-carousel-item
+              v-for="(img, index) in selectedPhoto.images"
+              :key="index"
+            >
+              <v-img :src="img" cover height="400" />
+            </v-carousel-item>
+          </v-carousel>
+        </div>
+        <v-card-title>{{ selectedPhoto?.title }}</v-card-title>
+
+        <v-card-subtitle>{{ selectedPhoto?.date }}</v-card-subtitle>
+
+        <v-card-text v-if="selectedPhoto?.description">
+          {{ selectedPhoto.description }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" text @click="showModal = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
 
 // Dummy data
 const photos = ref([
@@ -36,35 +74,50 @@ const photos = ref([
     id: 1,
     title: 'First Photo',
     date: '2025-02-10',
-    image: 'https://picsum.photos/600/400?1'
+    description: 'description01',
+    images: [
+      'https://picsum.photos/600/400?1',
+      'https://picsum.photos/600/400?11',
+      'https://picsum.photos/600/400?12',
+    ],
   },
   {
     id: 2,
     title: 'Second Photo',
     date: '2025-02-09',
-    image: 'https://picsum.photos/600/400?2'
+    description: 'description02',
+    images: ['https://picsum.photos/600/400?2', 'https://picsum.photos/600/400?21'],
   },
   {
     id: 3,
     title: 'Third Photo',
     date: '2025-02-08',
-    image: 'https://picsum.photos/600/400?3'
+    description: 'description03',
+    images: ['https://picsum.photos/600/400?3'],
   },
   {
     id: 4,
     title: 'Fourth Photo',
     date: '2025-02-07',
-    image: 'https://picsum.photos/600/400?4'
-  }
+    description: 'description04',
+    images: ['https://picsum.photos/600/400?5'],
+  },
 ])
 
+const showModal = ref(false)
+const selectedPhoto = ref(null)
+const hovering = ref(false)
+
 function openPhoto(photo) {
-  alert(`Clicked: ${photo.title}`)
+  selectedPhoto.value = photo
+  showModal.value = true
 }
+
+const { smAndUp } = useDisplay()
 </script>
 
 <style scoped>
-.cursor-pointer {
-  cursor: pointer;
-}
+  .cursor-pointer {
+    cursor: pointer;
+  }
 </style>
