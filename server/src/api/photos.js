1c624089f0e98b8748f4f641a43f6ae3ab3b82c8
Eno951024 <kamo8352@gmail.com>
2026-04-21 12:13:30 +0900

    サーバーリポジトリの分離

--- server/src/api/photos.js
+++
@@ -1,9 +0,0 @@
-export async function getPhotos() {
-  const res = await fetch('http://localhost:3001/photos');
-  
-  if (!res.ok) {
-    throw new Error('API request failed');
-  }
-
-  return await res.json();
-}
